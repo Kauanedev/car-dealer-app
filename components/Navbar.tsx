c9efdeaa66c5aeda9db80d053aec82b4e09d6b1b
Kauane Santos <kauanesantos.coding@gmail.com>
2024-08-20 18:20:52 -0300

    feat: create navbar and footer

---
+++ components/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import CustomButton from "./CustomButton";
+
+const Navbar = () => {
+  return (
+<div></div>
+  )
+};
+
+export default Navbar;

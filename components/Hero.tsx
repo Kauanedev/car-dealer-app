339844300b6ef907e326d28e07534e151b7e0f9a
Kauane Santos <kauanesantos.coding@gmail.com>
2024-08-20 17:48:42 -0300

    feat: add hero img

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import React from 'react'
-import image from 'next/image'
+import Image from 'next/image'
 import CustomButton from './CustomButton'
 
 const Hero = () => {
@@ -21,6 +21,13 @@ const Hero = () => {
         containerStyles="bg-primary-blue text-white rounded-full mt-10"
         handleClick={handleScroll}
         />
+        </div>
+
+        <div className="hero__image-container">
+            <div className="div hero__image">
+                <Image src="/hero.png" alt="hero" fill className='object-contain'/>
+                <div className="hero__image-overlay"/>
+            </div>
         </div>
     </div>
   )

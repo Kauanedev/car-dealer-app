26c1301a5a0fb7b9da18e655c50a84dd7a40136b
Kauane Santos <kauanesantos.coding@gmail.com>
2024-08-20 17:13:55 -0300

    feat: create hero page

---
+++ components/Hero.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import image from 'next/image'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+        <div className="flex-1 pt-36 padding-x">
+            <h1 className='hero__title'>Find, Book, or Own Your Next Car</h1>
+            <p className='hero__subtitle'>Discover the best deals and offer your dream car today!</p>
+
+        </div>
+    </div>
+  )
+}
+
+export default Hero

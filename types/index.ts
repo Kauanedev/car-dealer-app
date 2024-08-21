a8c4641c035b315d50e4de498763e577dc3d2201
Kauane Santos <kauanesantos.coding@gmail.com>
2024-08-21 13:08:08 -0300

    feat: create vehicles types

--- types/index.ts
+++ types/index.ts
@@ -4,4 +4,15 @@ export interface CustomButtonProps {
   title: string;
   containerStyles: string;
   handleClick?: MouseEventHandler<HTMLButtonElement>;
+  btnType?: "button" | "submit";
+}
+
+export interface SearchVehicleTypesProps {
+  vehicleTypes: string;
+  setVehicleTypes: (vehicleTypes: string) => void;
+}
+export interface VehicleTypesProps {
+  ModelId: string;
+  MakeName: string;
+  ModelName: string;
 }

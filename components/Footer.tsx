0f4536c02c2815d7193a478f23b3a8a35593898f
Kauane Santos <kauanesantos.coding@gmail.com>
2024-08-20 22:16:05 -0300

    feat: create footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="flex flex-col text-black-100 mt-5 border-t border-gray-100">
+      <div className="flex max-md:flex-col flex-wrap justify-between ga-5 sm:px-16 px-6 py-10">
+        <p className="text-base text-gray-700">
+          @2024 Car Dealer App. All rights reserved
+        </p>
 
-export default Footer
+        <div className="footer__copyrights-link">
+          <Link href="/" className="text-gray-500">
+            Privacy Policy
+          </Link>
+          <Link href="/" className="text-gray-500">
+            Terms of Use
+          </Link>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;

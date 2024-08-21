5e7f3a319dcabac44ad9e336415a4367d9ec448b
Kauane Santos <kauanesantos.coding@gmail.com>
2024-08-20 22:15:49 -0300

    feat: add sign in buton + hero prettier

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,36 +1,35 @@
-"use client"
+"use client";
 
-import React from 'react'
-import Image from 'next/image'
-import CustomButton from './CustomButton'
+import React from "react";
+import Image from "next/image";
+import CustomButton from "./CustomButton";
 
 const Hero = () => {
-
-  const handleScroll = () => {
-
-  };
+  const handleScroll = () => {};
 
   return (
-    <div className='hero'>
-        <div className="flex-1 pt-36 padding-x">
-            <h1 className='hero__title'>Find, Book, or Own Your Next Car</h1>
-            <p className='hero__subtitle'>Discover the best deals and offer your dream car today!</p>
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">Find, Book, or Own Your Next Car</h1>
+        <p className="hero__subtitle">
+          Discover the best deals and offer your dream car today!
+        </p>
 
         <CustomButton
-        title="Explore Cars"
-        containerStyles="bg-primary-blue text-white rounded-full mt-10"
-        handleClick={handleScroll}
+          title="Explore Cars"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
         />
-        </div>
+      </div>
 
-        <div className="hero__image-container">
-            <div className="div hero__image">
-                <Image src="/hero.png" alt="hero" fill className='object-contain'/>
-                <div className="hero__image-overlay"/>
-            </div>
+      <div className="hero__image-container">
+        <div className="div hero__image">
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
+          <div className="hero__image-overlay" />
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
